398fdca7408618f7e9eb5a0f0f87c78453cf9132
Matt Readout <matt.readout@gmail.com>
2019-05-01 16:57:01 -0400

    build user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,35 @@
 class Api::V1::UsersController < ApplicationController
+	before_action :find_user, only: [:show]
+
+	def index
+	  @users = User.all
+		render json: @users
+	end
+
+	def show
+	  render json: @user
+	end
+
+	def new
+	  @user = User.new
+	end
+
+	def create
+	  @user = User.create(user_params)
+		if @user.valid?
+			render json: @user
+		else
+			render json: { error: 'failed to create user' }, status: :not_acceptable
+		end
+	end
+
+	private
+
+	def user_params
+	  params.permit(:username, :password, :password_confirmation)
+	end
+
+	def find_user
+	  @user = User.find(params[:id])
+	end
 end
